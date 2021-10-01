9867872affa0223469cac17e42fe4901fee7a3f7
Cintia Várhegyi <cintia.varhegyi@stud.uni-corvinus.hu>
2021-10-01 10:42:09 +0200

    Create default UI

---
+++ UserMaintanance/UserMaintanance/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserMaintanance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblLastName.Text = Resource1.LastName;
+            lblFirstName.Text = Resource1.FirstName;
+            btnAdd.Text = Resource1.Add;
+        }
+    }
+}
